0a659bec0cc60bafbdf3378bd59a80a43946caf9
perlaescano <perlaescanoe@gmail.com>
2022-04-09 00:57:17 -0400

    Init Credits and Debits sections

--- bank-react-app/src/App.js
+++ bank-react-app/src/App.js
@@ -1,12 +1,16 @@
-import React, { Component }  from 'react';
+import React from "react"; 
 import './App.css';
+import Content from './components/Content';
 import Clock from './components/Clock';
+
 
 function App() {
   return (
     <div className="App">
       <p>Hello User</p>
       <Clock></Clock>
+      
+			<Content></Content>
     </div>
   );
 }
