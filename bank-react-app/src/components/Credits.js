95a400a22489015ba767d57c9ce7b4241920bbc0
perlaescano <perlaescanoe@gmail.com>
2022-04-09 01:12:49 -0400

    Fetches data from API

--- bank-react-app/src/components/Credits.js
+++ bank-react-app/src/components/Credits.js
@@ -1,6 +1,18 @@
-import React from 'react';
 
+import React, { useEffect } from 'react';
 const Credits = () => {
+    useEffect(() => {
+        fetch('https://moj-api.herokuapp.com/credits')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+    }, []);
+
+
+    
   return (
     <div>
       <h1>Credits</h1>
