a4106e332dca3bb82911a0d900489f3007e83ba6
Eric Dittus <ericdittus@Erics-MacBook-Air.local>
2022-04-19 03:33:49 -0400

    can change background and text color now. The only issue is changing the background color for the clock header

--- bank-react-app/src/components/UserProfile.js
+++ bank-react-app/src/components/UserProfile.js
@@ -42,32 +42,48 @@ function UserProfile(){
             var mingzi= document.getElementsByClassName("userNameform-control");
             
             const headerName = document.getElementById("starterInfo_username");
+            const firstheaderName =  document.getElementById("firstUserNameUse");
             if (mingzi.length > 0) {
                 headerName.textContent = "Username: " +  mingzi[0].value;
+                firstheaderName.textContent = "Hello " + mingzi[0].value;
+            }
+
+            var colorForBackground = document.getElementsByClassName("backColorform-control");
+            if(colorForBackground.length > 0){
+                document.getElementById("changeBackground").style.backgroundColor = colorForBackground[0].value;
+            }
+
+            var colorForText = document.getElementsByClassName("textColorform-control");
+            if(colorForText.length > 0){
+                document.getElementById("changeBackground").style.color = colorForText[0].value;
+                document.getElementById("callapsableButton").style.color = colorForText[0].value;
+                document.getElementById("buttons1").style.color = colorForText[0].value;
+                document.getElementById("buttons2").style.color = colorForText[0].value;
             }
             
             
             //if(userName.formData!=null){uName = userName.formData;}
-            console.log(uName);
+            //console.log(uName);
 
             
         };
             return (
-            <body style = {{backgroundColor: {backColor},textColor: {texColor}}} >
+                
+            <body id = "changeBackground" style = {{backgroundColor: "white",color: "black",height:"1000px"}} >
                 <div style={user}>
-                    <h1>Hello {uName}</h1>
+                    <h1 id = "firstUserNameUse" >Hello {uName}</h1>
                     <div id="starterInfo">
                         <div id = "starterInfo_username">Username: {uName}</div>
                         <div>Member Since: {memSince}</div>
                     </div>
                     <div id="customizeProfileCallapse">
-                        <div className ="userSettings" {...getToggleProps({onClick: handleOnClick})}>
-                        <button>{isExpanded ? 'Collapse' : 'User Settings'}</button>
+                        <div className ="userSettings" {...getToggleProps({onClick: handleOnClick})} style = {{marginTop: "10px"}}>
+                        <button id = "callapsableButton">{isExpanded ? 'Collapse' : 'User Settings'}</button>
 
                         </div>
                         <div {...getCollapseProps()}>
                             <div className="content">
-                            <div className="userSettings">
+                            <div className="userSettings" >
                             <section className="heading">
                                 <h1>User Settings</h1>
                             </section>
@@ -114,7 +130,7 @@ function UserProfile(){
                                     
                                 </div>
 
-                    <div>
+                    <div style = {{marginTop: "10px"}}>
                         <button>Submit</button>
                      </div>
                 </form>
@@ -128,8 +144,8 @@ function UserProfile(){
 
                     <div id="loginMenu">
                         <h4>Menu</h4>
-                        <Link id="buttons" to="/credit"><button><p>View Credit</p></button></Link>
-                        <Link id="buttons" to="/debit"><button><p>View Debits</p></button></Link>
+                        <Link id="buttons" to="/credit"><button><p id = "buttons1">View Credit</p></button></Link>
+                        <Link id="buttons" to="/debit"><button><p id = "buttons2">View Debits</p></button></Link>
                     </div>
                 </div>
             </body>
@@ -147,7 +163,7 @@ const user ={
     width: "300px",
     backgroundColor: "beige",
     borderRadius: "10%",
-    boxShadow: "10px 10px 10px #dbdbd2",
+    //boxShadow: "10px 10px 10px #dbdbd2",
     paddingLeft: "20px",
     paddingTop: "10px",
     paddingRight: "20px",
